7da469e2797f147e0dd444175bd50466d6d8cfb7
Azalea (on HyDEV-Daisy) <me@hydev.org>
2022-05-06 01:20:03 -0400

    [+] Rebuild base

---
+++ build-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Stop on error
+set -e
+
+F=docker-compose.build.yml
+
+# Start image cache server so that you don't have to re-download the image for rebuilding
+# docker-compose build texlive-image
+docker-compose -f $F run texlive-image -d
+
+# Build base image
+docker-compose -f $F build nekoleaf-base
+
+# Build nekoleaf
+docker-compose -f $F build nekoleaf
+
+# Stop image cache server
+docker-compose -f $F stop texlive-image
